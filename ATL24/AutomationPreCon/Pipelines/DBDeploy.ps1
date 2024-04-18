1a531318670ec4c47a894f19b3c6dc521acf9107
Erin Dempster <erin.dempster@outlook.com>
2024-04-18 15:43:13 -0500

    updated deployment pipeline

---
+++ ATL24/AutomationPreCon/Pipelines/DBDeploy.ps1
@@ -0,0 +1,7 @@
+﻿. 'C:\Program Files\Microsoft SQL Server\160\DAC\bin\sqlpackage.exe' `
+        /action:Publish `
+        /sourceFile:$($env:Pipeline_Workspace)\AdventureWorks.dacpac `
+        /targetServerName:localhost `
+        /targetDatabaseName:AdventureWorksTest `
+        /targetTrustServerCertificate:true
+    
