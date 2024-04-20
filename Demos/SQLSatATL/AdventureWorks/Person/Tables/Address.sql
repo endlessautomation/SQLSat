69bfb4490a47318246c89500d11b350c7762f18d
Erin Dempster <erin.dempster@outlook.com>
2024-04-20 12:48:40 -0500

    Task 1234 - Lengthen AddressLine2

--- Demos/SQLSatATL/AdventureWorks/Person/Tables/Address.sql
+++ Demos/SQLSatATL/AdventureWorks/Person/Tables/Address.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [Person].[Address] (
     [AddressID]       INT               IDENTITY (1, 1) NOT FOR REPLICATION NOT NULL,
-    [AddressLine1]    NVARCHAR (100)    NOT NULL,
+    [AddressLine1]    NVARCHAR (200)    NOT NULL,
     [AddressLine2]    NVARCHAR (60)     NULL,
     [City]            NVARCHAR (30)     NOT NULL,
     [StateProvinceID] INT               NOT NULL,
